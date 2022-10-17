27fed35c0d10c9047bd5bf927b2eebace971c8c1
Oleg <myaddub@gmail.com>
2022-10-17 13:55:06 +0300

    init()

---
+++ CloneInstagram/CloneInstagram/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  CloneInstagram
+//
+//  Created by Oleg_Yakovlev on 17.10.22.
+//
+
+import UIKit
+
+///
+class ViewController: UIViewController {
+    // MARK: - Private IBoutlet
+
+    // MARK: - Private Visual Components
+
+    // MARK: - Public Properties
+
+    // MARK: - Private Properties
+
+    // MARK: - Initializers
+
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Public Methods
+
+    // MARK: - Private IBAction
+
+    // MARK: - Private Methods
+
+}
